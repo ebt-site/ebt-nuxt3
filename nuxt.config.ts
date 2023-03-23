f81179a578c82a09f053f8c80b9189a49aac4633
Karl Lew <karl@firepick.org>
2023-03-23 10:02:31 -0700

    v += prerender HTML with nuxi generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  //ssr: false,
+  //nitro: {
+    //preset: 'service-worker'
+  //},
   modules: ['@nuxt/content']
 })
