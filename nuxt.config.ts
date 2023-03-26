292a4fa972de9615ab3a2237b78e784470221181
Karl Lew <karl@firepick.org>
2023-03-26 13:50:53 -0700

    v0.6.0 += ssr:true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   modules: ['@nuxt/content'],
   app: {
     baseURL: '/ebt-nuxt3/',
