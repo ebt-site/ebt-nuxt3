f4dc954918a68e7e8f66c2777141e5a3a49895d2
Karl Lew <karl@firepick.org>
2023-03-26 11:15:42 -0700

    v0.2.0 += scripts

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,25 @@
+SCRIPT=`basename $0`
+echo -e "$SCRIPT: BEGIN"
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ "$BRANCH" != "gh-pages" ]; then
+  git checkout gh-pages; git pull
+fi
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ "$BRANCH" != "gh-pages" ]; then
+  echo -e "$SCRIPT: ERROR could not checkout gh-pages"
+  exit -1
+fi
+echo -e "$SCRIPT: git branch: $BRANCH"
+
+echo -e "$SCRIPT: removing existing content"
+rm *html favicon.ico _payload.js
+rm -rf about api bad_links dist index _nuxt testdir
+
+echo -e "$SCRIPT: copying new content"
+cp -r .output/public/* .
+ls -l
+git add
+git commit -m "gh-pages"
+
+echo -e "$SCRIPT END"
