21ab8dfa820c8a6abdbe34a91b99dc0cb85425e4
Karl Lew <karl@firepick.org>
2023-03-29 09:07:19 -0700

    v0.8.0 += gh-pages

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,12 +1,13 @@
 SCRIPT=`basename $0`
 echo -e "$SCRIPT: BEGIN"
 
+echo -e "$SCRIPT: generating static build..."
 git checkout main
+npm run clean
+npm run generate
 
-BRANCH=`git rev-parse --abbrev-ref HEAD`
-if [ "$BRANCH" != "gh-pages" ]; then
-  git checkout gh-pages; git pull
-fi
+echo -e "$SCRIPT git checkout gh-pages"
+git checkout gh-pages; git pull
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 if [ "$BRANCH" != "gh-pages" ]; then
   echo -e "$SCRIPT: ERROR could not checkout gh-pages"
@@ -21,6 +22,8 @@ rm -rf about api bad_links dist index _nuxt testdir
 echo -e "$SCRIPT: copying new content"
 cp -r .output/public/* .
 ls -l
+
+echo -e "$SCRIPT: updating github"
 git add .
 git commit -m "gh-pages"
 
