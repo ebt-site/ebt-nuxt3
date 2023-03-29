7b27c76fe2b7016af4a7f538a235098e3372a910
Karl Lew <karl@firepick.org>
2023-03-29 09:01:11 -0700

    v0.8.0 += vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // Github pages
   ssr: true,
   modules: ['@nuxt/content'],
   app: {
     baseURL: '/ebt-nuxt3/',
   },
+
+  // Vuetify configuration
+  css: [
+    '@mdi/font/css/materialdesignicons.min.css',
+    'vuetify/lib/styles/main.sass'
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+
 })
